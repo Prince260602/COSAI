57372ad6441e897999c73c95a48d5250830eea4a
Prince <princemultaniya89@gmail.com>
2025-02-12 02:56:37 +0530

    fixed

--- Frontend/src/Pages/Dashboard/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -38,7 +38,7 @@ const Dashboard = () => {
     setLoading(true); 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/questions/ask",
+        "https://cosai.onrender.com/api/questions/ask",
         { question: searchText },
         {
           headers: {
